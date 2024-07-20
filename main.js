e6079390e5b346e0ec09a1c1bca1f62e3936fe1c
Prantik Ghosh <prantik.g1996@gmail.com>
2024-07-20 17:34:13 +0530

    added theme toggler & changed placement of searchbar

---
+++ main.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const btn = document.getElementById("switch");
+const icon = btn.querySelector("i");
+
+let isToggled  = false;
+
+btn.addEventListener('click', () => {
+
+    isToggled = !isToggled;
+    body.style.setProperty("--valueBg", isToggled ? "#1f1f1f" : "#f3eded");
+    body.style.setProperty("--valueText", isToggled ? "#f3eded" : "#1f1f1f");
+    
+    icon.classList.toggle("bi-sunset", !isToggled);
+    icon.classList.toggle("bi-sunrise-fill", isToggled);
+    
+});
